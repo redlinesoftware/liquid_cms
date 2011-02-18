51d4548de51ce5abe18b35ffefdad3b0584880ce
Andrew Kaspick <andrew@redlinesoftware.com>
2011-02-17 22:31:12 -0600

    specify simple_form version in test app and increase rails version in test app

--- test/rails_app/config/environment.rb
+++ test/rails_app/config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -20,6 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'paperclip', :version => '~> 2.3.1'
+  config.gem 'simple_form', :version => '1.0.4'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
