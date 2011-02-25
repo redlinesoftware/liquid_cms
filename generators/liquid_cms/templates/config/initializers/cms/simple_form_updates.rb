3c2ffa94b5b141e68d2c13bb79351a64562f17e4
Andrew Kaspick <andrew@redlinesoftware.com>
2011-02-25 12:59:31 -0600

    fix translation error in forms and bump version to 0.2.0.11

--- generators/liquid_cms/templates/config/initializers/cms/simple_form_updates.rb
+++ generators/liquid_cms/templates/config/initializers/cms/simple_form_updates.rb
@@ -1,10 +1,10 @@
 class SimpleForm::FormBuilder
   def commit_button_or_cancel
-    @template.content_tag :div, :class => 'buttons' do
+    template.content_tag :div, :class => 'buttons' do
       String.new.tap do |html|
         html << button(:submit)
         html << " or "
-        html << template.link_to(t('simple_form.buttons.cancel'), :back, :class => 'cancel')
+        html << template.link_to(template.t('simple_form.buttons.cancel'), :back, :class => 'cancel')
       end
     end
   end
--- lib/liquid_cms/version.rb
+++ lib/liquid_cms/version.rb
@@ -1,3 +1,3 @@
 module Cms
-  VERSION = "0.2.0.10"
+  VERSION = "0.2.0.11"
 end
