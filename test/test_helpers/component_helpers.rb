a35516040ebcada6df9b5d5c60df56b6c20e2fa4
Andrew Kaspick <andrew@redlinesoftware.com>
2011-03-29 12:45:58 -0500

    additional component tests

--- test/test_helpers/component_helpers.rb
+++ test/test_helpers/component_helpers.rb
@@ -10,4 +10,26 @@ module ComponentHelpers
   def cleanup_components
     cleanup_files 'components'
   end
+
+  def create_zip(name)
+    fname = Rails.root.join('public', 'cms', 'temp').join(name)
+    FileUtils.mkdir_p File.dirname(fname)
+
+    begin
+      Zip::ZipFile.open(fname, Zip::ZipFile::CREATE) do |zipfile|
+        zipfile.get_output_stream("test.txt") {}
+        zipfile.get_output_stream("test.exe") {} # invalid file ext will be ignored
+        zipfile.mkdir("dir")
+        zipfile.get_output_stream("dir/test.exe") {}
+        zipfile.get_output_stream("dir/test.txt") {}
+        zipfile.get_output_stream("../../dir/test.exe") {} # relative path file will be ignored
+        zipfile.get_output_stream("../../dir/test.txt") {} # relative path file will be ignored 
+      end
+
+      yield fname
+
+    ensure
+      cleanup_files 'temp'
+    end
+  end
 end
