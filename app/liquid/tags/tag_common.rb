ee64985729a5899d0e4218d3b040b448e51d9aac
Andrew Kaspick <andrew@redlinesoftware.com>
2011-04-06 22:36:28 -0500

    modify declaration of module

--- app/liquid/tags/tag_common.rb
+++ app/liquid/tags/tag_common.rb
@@ -1,29 +1,31 @@
-module Cms::TagCommon
-  extend ActiveSupport::Memoizable
+module Cms
+  module TagCommon
+    extend ActiveSupport::Memoizable
 
-  HyphenatedTagAttributes = /([\w-]+)\s*\:\s*(#{Liquid::QuotedFragment})/
+    HyphenatedTagAttributes = /([\w-]+)\s*\:\s*(#{Liquid::QuotedFragment})/
 
-  def parse_options(context, markup)
-    begin
-      options = HashWithIndifferentAccess.new
-      return options if markup.blank?
+    def parse_options(context, markup)
+      begin
+        options = HashWithIndifferentAccess.new
+        return options if markup.blank?
 
-      markup.scan(HyphenatedTagAttributes) do |key, value|
-        options[key.to_sym] = context[value]
+        markup.scan(HyphenatedTagAttributes) do |key, value|
+          options[key.to_sym] = context[value]
+        end
+
+        options
+      rescue ArgumentError => e
+        raise SyntaxError.new("Syntax Error in 'tag options' - Valid syntax: name:value")
       end
-
-      options
-    rescue ArgumentError => e
-      raise SyntaxError.new("Syntax Error in 'tag options' - Valid syntax: name:value")
     end
-  end
 
-  def context_object(context)
-    context.registers[:context].object
-  end
-  memoize :context_object
+    def context_object(context)
+      context.registers[:context].object
+    end
+    memoize :context_object
 
-  def params(context)
-    context.registers[:controller].params.except(:controller, :action)
+    def params(context)
+      context.registers[:controller].params.except(:controller, :action)
+    end
   end
 end
